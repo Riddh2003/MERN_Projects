37398441a0a48b7d4a64eb71b12981856127f992
Riddh Modi <riddhmodi2003@gmail.com>
2025-01-05 15:49:52 +0530

    form

--- learning26/src/components/form/FormDemo.jsx
+++ learning26/src/components/form/FormDemo.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import {useForm} from "react-hook-form"
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form"
 
 export const FormDemo = () => {
-    const {register,handleSubmit} = useForm();
+    const { register, handleSubmit } = useForm();
+    const [userData,setUserData] = useState({});
+    const [isFormSubmitted,setIsFormSubmitted] = useState(false); 
 
-    const submitHandler = (data) =>{
+    const submitHandler = (data) => {
         console.log(data);
+        setUserData(data);
+        setIsFormSubmitted(true);
+
     }
     return (
         <div>
@@ -19,8 +24,34 @@ export const FormDemo = () => {
                     <label>AGE : </label>
                     <input type='Number' placeholder='Enter age' {...register("age")}></input>
                 </div>
+                <div>
+                    <label>GENDER : </label><br ></br>
+                    Male : <input type='radio' name='male' value='male' {...register("gender")}></input>
+                    Female : <input type='radio' name='female' value='female' {...register("gender")}></input>
+                </div>
+                <div>
+                    <select {...register("country")}>
+                        <option value="India">India</option>
+                        <option value="USA">USA</option>
+                        <option value="England">England</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Color : </label>
+                    <input type='color' {...register("color")}></input>
+                </div>
                 <input type='submit' placeholder='submit'></input>
             </form>
+            {
+                isFormSubmitted &&
+                <div style={{color:userData.color}}>
+                    <h1>Result</h1>
+                    <h2>Name: {userData.name}</h2>
+                    <h2>Age: {userData.age}</h2>
+                    <h2>Gender: {userData.gender}</h2>
+                    <h2>Country: {userData.country}</h2>
+                </div>
+            }
         </div>
     )
 }
