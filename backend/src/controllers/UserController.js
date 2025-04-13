56a1e8655adce686968f8de9f4497810515c8643
Riddh Modi <riddhmodi2003@gmail.com>
2025-04-13 14:58:42 +0530

    create apis.

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,20 +1,73 @@
-const getAllUsers = (req, res) => {
-    var users = [
-        {
-            id: 1,
-            name: 'Riddh'
-        },
-        {
-            id: 2,
-            name: 'Sumit'
-        }
-    ]
-    res.json({
-        message: "User Fetch.........",
-        data: users
-    })
+const userModel = require('../models/UserModel');
+
+const getAllUsers = async (req, res) => {
+
+    const users = await userModel.find();
+
+    if (users.length > 0) {
+        res.json({
+            message: "User Fetch.........",
+            data: users
+        })
+    } else {
+        res.json({
+            message: "User Data not Found.....",
+            data: []
+        })
+    }
+
 }
 
+const getUserById = async (req, res) => {
+    const foundUser = await userModel.findById(req.params.id);
+    if (foundUser) {
+        res.json({
+            message: "User Found",
+            data: foundUser
+        })
+    } else {
+        res.json({
+            message: "User not Found",
+            data: []
+        })
+    }
+}
+
+// const getUserByName = async (req, res) => {
+//     const foundUsers = await userModel.find({ name: req.params.name });
+//     if (foundUsers.length > 0) {
+//         res.json({
+//             message: "User Found",
+//             data: foundUsers
+//         })
+//     } else {
+//         res.json({
+//             message: "User not Found",
+//             data: []
+//         })
+//     }
+// }
+
+const getUserByName = async (req, res) => {
+    const query = req.query
+    console.log(query)
+    const foundUsers = await userModel.find({ name: req.query.name });
+    if (foundUsers.length > 0) {
+        res.json({
+            message: "User Found",
+            data: foundUsers
+        })
+    } else {
+        res.json({
+            message: "User not Found",
+            data: []
+        })
+    }
+}
+
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    getUserById,
+    getUserByName
 }
--- backend/src/routes/UserRoutes.js
+++ backend/src/routes/UserRoutes.js
@@ -5,4 +5,6 @@
 const router = require('express').Router();
 const userController = require('../controllers/UserController');
 router.get('/users', userController.getAllUsers);
+router.get('/user/:id', userController.getUserById)
+router.get('/usersbyname', userController.getUserByName)
 module.exports = router;
