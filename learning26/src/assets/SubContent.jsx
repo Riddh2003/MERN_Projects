04cce83f6a925d84cf3c71c9b1b701ecd3d8051f
Riddh Modi <riddhmodi2003@gmail.com>
2025-05-12 23:45:31 +0530

    create new project using npx

--- learning26/src/assets/SubContent.jsx
+++ learning26/src/assets/SubContent.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const SubContent = (props) => {
   return (
     <div>
-        <h1>{props.val}</h1>
-        <h1>{props.str}</h1>
+      <h1>{props.val}</h1>
+      <h1>{props.str}</h1>
     </div>
   )
 }
