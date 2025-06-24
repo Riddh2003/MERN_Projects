6d43e07afc85c40dc9ab683b9b9eab78cdf44ba3
Riddh Modi <riddhmodi2003@gmail.com>
2025-06-24 09:52:01 +0530

    upload

--- homework/src/components/FileUploading.jsx
+++ homework/src/components/FileUploading.jsx
@@ -1,21 +1,36 @@
-import axios from 'axios';
 import React from 'react'
+import axios from 'axios';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form'
 
 export const FileUploading = () => {
     const { register, handleSubmit } = useForm();
     const [image, setImage] = useState();
+
+
     const submitHandler = async (data) => {
-        const respoonse = await axios.post('http://localhost:3000/upload/files?id=6831bafb012f2de0200576ea', {
-            photos: data.files
-        })
-        setImage(respoonse);
-        console.log(respoonse);
+        const formData = new FormData();
+        for (let i = 0; i < data.files.length; i++) {
+            formData.append('photos', data.files[i]);
+        }
+        try {
+            const respoonse = await axios.post('http://localhost:3000/upload/files?id=6831bafb012f2de0200576ea', formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            )
+            console.log(data);
+            console.log(respoonse.data.cloudinaryResponse.secure_url);
+            setImage(respoonse.data.cloudinaryResponse.secure_url);
+        } catch (error) {
+            console.log(error);
+        }
     }
     return (
         <div>
-            <form onClick={handleSubmit(submitHandler)}>
+            <form onSubmit={handleSubmit(submitHandler)}>
                 <div>
                     <label>FileUpload</label>
                     <input multiple type='file' {...register('files')}></input>
@@ -26,7 +41,7 @@ export const FileUploading = () => {
                 </div>
                 <button type='submit' className='p-2 bg-blue-300 rounded-lg'>Upload</button>
             </form>
-            {/* <img src={ }></img> */}
+            <img src={image} alt='upload'></img>
         </div >
     )
 }
